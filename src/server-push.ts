c21e51e3af6295a262fb13abf3497f65103433ff
Carl-Hugo Marcotte <carlhugo@gmail.com>
2022-08-29 22:25:40 -0400

    Broadcast createAndShowTemporaryJournalEntry

--- src/server-push.ts
+++ src/server-push.ts
@@ -19,8 +19,23 @@ await game.experiments.showJournalEntryById(journalEntryId);
 //new JournalEntry(note).show()
 
 export class ServerPush implements IOgModule {
-    ready(): void {}
-    init() {
+    ready(): void {
+        logText('ServerPush getting ready');
+
+        let connection = new HubConnectionBuilder().withUrl('https://localhost:7263/journal-entry').build();
+
+        connection.on('pong', () => {
+            logText('pong');
+        });
+
+        connection.on('createShowAndDeleteNewJournalEntry', this.createShowAndDeleteNewJournalEntry);
+        connection.on('createAndShowTemporaryJournalEntry', this.createAndShowTemporaryJournalEntry);
+
+        connection.start().then(() => connection.invoke('Ping'));
+
+        logText('ServerPush is ready');
+    }
+    init(): void {
         logText('ServerPush initiating');
         // Javascript
         // game.experiments.showJournalEntryById = async function (journalEntryId) {
@@ -52,15 +67,65 @@ export class ServerPush implements IOgModule {
         //     // JournalEntry.create({name: "Journal name", pages:[{type: "text", name: "Quest hook", text:{content: `HTML content here`}}]})
         // };
 
-        let connection = new HubConnectionBuilder().withUrl('https://localhost:7263/journal-entry').build();
-
-        connection.on('pong', () => {
-            logText('pong');
-        });
-
-        connection.start().then(() => connection.invoke('Ping'));
-
         logText('ServerPush initiated');
+    }
+
+    async createAndShowTemporaryJournalEntry(options: ICreateAndShowTemporaryJournalEntry): Promise<void> {
+        const entry = await JournalEntry.create(
+            {
+                name: options.name,
+                content: options.content,
+                permission: foundry.CONST.DOCUMENT_PERMISSION_LEVELS.OBSERVER,
+            },
+            { temporary: true, renderSheet: true }
+        );
+        if (entry === undefined) {
+            console.error('No entry was created.');
+            return;
+        }
+        console.log(`Journal entry '${options.name}' created.`);
+        // await entry.show('text', true);
+        await entry.sheet.render(true);
+    }
+
+    async createShowAndDeleteNewJournalEntry(options: ICreateShowAndDeleteNewJournalEntry): Promise<void> {
+        const entry = await JournalEntry.create({
+            name: options.name,
+            content: options.content,
+        });
+        if (entry === undefined) {
+            console.error('No entry was created.');
+            return;
+        }
+        console.log(`Journal entry '${options.name}' created with isPermanent = ${options.isPermanent}.`);
+        await entry.show('text', true);
+
+        if (options.isPermanent) {
+            return;
+        }
+        const deleteEntryInMS = options.deleteDelay || 60000;
+        console.log(`Scheduling journal entry deletion in ${deleteEntryInMS} ms.`);
+        setTimeout(async () => {
+            console.log('Deleting journal entry', entry);
+            await entry.delete();
+            console.log('Journal entry deleted');
+        }, deleteEntryInMS);
+
+        // V10 multi-page syntax
+        // JournalEntry.create({name: "Journal name", pages:[{type: "text", name: "Quest hook", text:{content: `HTML content here`}}]})
     }
 }
 // await game.experiments.showNewJournalEntry('Invented note', '<p>Some cool text with <strong>bold content</strong>.</p>');
+
+interface ICreateShowAndDeleteNewJournalEntry {
+    name: string;
+    content: string;
+    deleteDelay?: number;
+    isPermanent: boolean;
+}
+interface ICreateAndShowTemporaryJournalEntry {
+    name: string;
+    content: string;
+    deleteDelay?: number;
+    isPermanent: boolean;
+}
