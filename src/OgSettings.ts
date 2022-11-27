4224341686b1d8c53fa96775a09c3e501839a344
Carl-Hugo Marcotte <carlhugo@gmail.com>
2022-11-27 14:32:40 -0500

    Clean up

--- src/OgSettings.ts
+++ src/OgSettings.ts
@@ -2,15 +2,6 @@ import { logText } from './utils';
 
 export const namespace = 'og-experiments';
 
-// type TCtor: T extends string
-//     ? typeof String
-//     : T extends number
-//     ? typeof Number
-//     : T extends boolean
-//     ? typeof Boolean
-//     : T extends Array<any>
-//     ? typeof Array
-//     : ConstructorOf<T>
 export class OgSetting<T> {
     private _value: T;
     constructor(
@@ -44,7 +35,3 @@ export class OgSetting<T> {
         (game as Game).settings.set(namespace, this.key, value);
     }
 }
-
-//type union = ClientSettings.Values[`${N}.${K}`] extends string | number | boolean | Array<any> | object
-// ? ClientSettings.PartialSettingConfig<ClientSettings.Values[`${N}.${K}`]>
-// : ClientSettings.PartialSettingConfig<T>;
