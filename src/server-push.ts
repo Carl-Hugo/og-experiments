15bbac93a6896051148e6b3ddf8bcfc131e3a3b0
Carl-Hugo Marcotte <carlhugo@gmail.com>
2022-11-27 16:58:12 -0500

    Update createAndShowTemporaryJournalEntry to v10

--- src/server-push.ts
+++ src/server-push.ts
@@ -2,7 +2,7 @@
 import { HubConnectionBuilder } from '@microsoft/signalr';
 import { IOgModule } from './IModule';
 import Keycloak, { KeycloakAdapter } from 'keycloak-js';
-import { addGameExtensions, logText } from './utils';
+import { addGameExtensions, logError, logText } from './utils';
 
 // function logText(text: string) {
 //     console.debug('og-experiments | ' + text);
@@ -94,13 +94,17 @@ export class ServerPush implements IOgModule {
             user,
             ping: () => connection.invoke('Ping'),
         });
+        addGameExtensions('flow', {
+            createAndShowTemporaryJournalEntry: this.createAndShowTemporaryJournalEntry,
+        });
 
         connection.on('pong', () => {
             logText('pong');
         });
 
-        connection.on('createShowAndDeleteNewJournalEntry', this.createShowAndDeleteNewJournalEntry);
-        connection.on('createAndShowTemporaryJournalEntry', this.createAndShowTemporaryJournalEntry);
+        // connection.on('createShowAndDeleteNewJournalEntry', this.createShowAndDeleteNewJournalEntry);
+        // connection.on('createAndShowTemporaryJournalEntry', this.createAndShowTemporaryJournalEntry);
+
         connection.on('execute', this.execute);
         connection.on('executeAsync', this.executeAsync);
 
@@ -147,47 +151,58 @@ export class ServerPush implements IOgModule {
         const entry = await JournalEntry.create(
             {
                 name: options.name,
-                content: options.content,
-                // permission: foundry.CONST.DOCUMENT_PERMISSION_LEVELS.OBSERVER,
-                permissions: foundry.CONST.DOCUMENT_PERMISSION_LEVELS.OBSERVER,
+                pages: [
+                    {
+                        name: options.name,
+                        type: 'text',
+                        text: {
+                            content: options.content,
+                        },
+                        // @ts-ignore
+                        ownership: { default: CONST.DOCUMENT_OWNERSHIP_LEVELS.OBSERVER },
+                    },
+                ],
+                // @ts-ignore
+                ownership: { default: CONST.DOCUMENT_OWNERSHIP_LEVELS.OBSERVER },
             },
             { temporary: true, renderSheet: true }
         );
-        if (entry === undefined) {
-            console.error('No entry was created.');
+
+        if (!entry) {
+            logError('No entry was created.');
             return;
         }
-        logText(`Journal entry '${options.name}' created.`);
-        // await entry.show('text', true);
+
+        logText(`Journal entry '${options.name}' created.`, entry);
         await entry.sheet?.render(true);
     }
 
-    async createShowAndDeleteNewJournalEntry(options: ICreateShowAndDeleteNewJournalEntry): Promise<void> {
-        const entry = await JournalEntry.create({
-            name: options.name,
-            content: options.content,
-        });
-        if (entry === undefined) {
-            console.error('No entry was created.');
-            return;
-        }
-        logText(`Journal entry '${options.name}' created with isPermanent = ${options.isPermanent}.`);
-        await entry.show('text', true);
+    // async createShowAndDeleteNewJournalEntry(options: ICreateShowAndDeleteNewJournalEntry): Promise<void> {
+    //     const entry = await JournalEntry.create({
+    //         name: options.name,
+    //         content: options.content,
+    //     });
+    //     if (entry === undefined) {
+    //         console.error('No entry was created.');
+    //         return;
+    //     }
+    //     logText(`Journal entry '${options.name}' created with isPermanent = ${options.isPermanent}.`);
+    //     await entry.show('text', true);
 
-        if (options.isPermanent) {
-            return;
-        }
-        const deleteEntryInMS = options.deleteDelay || 60000;
-        logText(`Scheduling journal entry deletion in ${deleteEntryInMS} ms.`);
-        setTimeout(async () => {
-            console.debug('Deleting journal entry', entry);
-            await entry.delete();
-            logText('Journal entry deleted');
-        }, deleteEntryInMS);
+    //     if (options.isPermanent) {
+    //         return;
+    //     }
+    //     const deleteEntryInMS = options.deleteDelay || 60000;
+    //     logText(`Scheduling journal entry deletion in ${deleteEntryInMS} ms.`);
+    //     setTimeout(async () => {
+    //         console.debug('Deleting journal entry', entry);
+    //         await entry.delete();
+    //         logText('Journal entry deleted');
+    //     }, deleteEntryInMS);
 
-        // V10 multi-page syntax
-        // JournalEntry.create({name: "Journal name", pages:[{type: "text", name: "Quest hook", text:{content: `HTML content here`}}]})
-    }
+    //     // V10 multi-page syntax
+    //     // JournalEntry.create({name: "Journal name", pages:[{type: "text", name: "Quest hook", text:{content: `HTML content here`}}]})
+    // }
 
     async execute(options: ExecuteOptions, user: ExecuteUser): Promise<void> {
         logText('ServerPush.execute', options, user);
@@ -198,19 +213,17 @@ export class ServerPush implements IOgModule {
         await new Promise((resolve, reject) => eval(options.command));
     }
 }
-// await game.experiments.showNewJournalEntry('Invented note', '<p>Some cool text with <strong>bold content</strong>.</p>');
 
-interface ICreateShowAndDeleteNewJournalEntry {
-    name: string;
-    content: string;
-    deleteDelay?: number;
-    isPermanent: boolean;
-}
+// interface ICreateShowAndDeleteNewJournalEntry {
+//     name: string;
+//     content: string;
+//     deleteDelay?: number;
+//     isPermanent: boolean;
+// }
 interface ICreateAndShowTemporaryJournalEntry {
     name: string;
     content: string;
-    deleteDelay?: number;
-    isPermanent: boolean;
+    ownership: number;
 }
 
 interface ExecuteOptions {
