2757f6648d0b1beae7fdf52dcf74bf26570e710a
Carl-Hugo Marcotte <carlhugo@gmail.com>
2022-12-23 10:15:42 -0500

    Fine tuning the crawl

--- src/Crawl.ts
+++ src/Crawl.ts
@@ -3,6 +3,8 @@ import { namespace } from './OgSettings';
 import { OgGameModuleSocket } from './OgGameModuleSocket';
 import { logError, logText, registerGameExtensions } from './utils';
 
+const crawlUrlPrefix = 'https://crawls.rpg.solutions/crawls/play/';
+
 const enricherName = 'StarWarsCrawl';
 const iframe = document.createElement('iframe');
 iframe.width = '100%';
@@ -11,23 +13,13 @@ iframe.style.zIndex = '999';
 iframe.style.position = 'absolute';
 iframe.style.border = '0px none';
 
-const crawlIcon = 'fa-regular fa-projector'; //<i class="fa-brands fa-galactic-republic"></i>
-const openCrawlIcon = 'fa-regular fa-power-off'; //<i class="fa-light fa-presentation-screen"></i>
+const crawlIcon = 'fa-regular fa-projector';
+const openCrawlIcon = 'fa-regular fa-power-off';
 const closeCrawlIcon = 'fa-regular fa-circle-xmark';
 const playCrawlIcon = 'fa-regular fa-play';
 
 const crawlIconElement = document.createElement('i');
 crawlIconElement.className = crawlIcon;
-
-const closeButton = document.createElement('a');
-closeButton.dataset['type'] = enricherName;
-closeButton.dataset['socketAction'] = 'close';
-closeButton.style.zIndex = '1000';
-closeButton.style.position = 'absolute';
-closeButton.style.top = '0';
-closeButton.style.right = '0';
-closeButton.classList.add('content-link');
-closeButton.innerHTML = `<i class="${crawlIcon}"></i><i class="${closeCrawlIcon}"></i> Close`;
 
 export class StarWarsCrawl implements IOgModule {
     private ogGameModuleSocket = new OgGameModuleSocket(enricherName);
@@ -35,8 +27,8 @@ export class StarWarsCrawl implements IOgModule {
         logText('StarWarsCrawl initiating');
 
         registerGameExtensions('crawl', {
-            loadCrawl,
-            unloadCrawl,
+            loadCrawl: this.loadCrawl,
+            unloadCrawl: this.unloadCrawl,
         });
 
         (CONFIG as any).TextEditor.enrichers.push({
@@ -47,7 +39,7 @@ export class StarWarsCrawl implements IOgModule {
                 const data = {
                     name: name,
                     icon: 'fas fa-link',
-                    classes: ['content-link'],
+                    classes: ['content-link', 'og-crawl-button'],
                     dataset: {
                         uuid: `${enricherName}.${url}`,
                         id: url,
@@ -55,27 +47,30 @@ export class StarWarsCrawl implements IOgModule {
                         broken: false,
                         tooltip: 'tmp',
                         url: url,
+                        ogCrawlPlayerName: name,
                     },
                 };
 
                 const container = document.createElement('span');
                 container.classList.add('og-crawl-player');
+                container.dataset.ogCrawlPlayerName = data.name;
                 container.appendChild(crawlIconElement);
                 container.appendChild(document.createTextNode(` ${data.name} `));
-                container.appendChild(CreateButton('open', `Open « ${data.name} » for everyone`, 'Open', openCrawlIcon));
-                container.appendChild(CreateButton('play', `Play « ${data.name} » for everyone`, 'Play', playCrawlIcon, true));
-                container.appendChild(CreateButton('close', `Close « ${data.name} » for everyone`, 'Close', closeCrawlIcon));
+                container.appendChild(CreateButton(CrawlActions.open, `Open « ${data.name} » for everyone`, 'Open', openCrawlIcon));
+                container.appendChild(CreateButton(CrawlActions.play, `Play « ${data.name} » for everyone`, 'Play', playCrawlIcon, true));
+                container.appendChild(CreateButton(CrawlActions.close, `Close « ${data.name} » for everyone`, 'Close', closeCrawlIcon));
                 return container;
 
                 function CreateButton(
-                    socketAction: string,
+                    socketAction: CrawlActions,
                     tooltip: string,
                     text: string,
                     iconName: string,
                     disabled = false
                 ): HTMLElement {
-                    const button = document.createElement('a');
+                    const button = document.createElement('button');
                     button.classList.add(...data.classes);
+                    button.classList.add(`og-crawl-button-${socketAction}`);
                     button.draggable = false;
                     for (let [k, v] of Object.entries(data.dataset)) {
                         button.dataset[k] = v;
@@ -84,6 +79,7 @@ export class StarWarsCrawl implements IOgModule {
                     icon.className = iconName;
                     button.appendChild(icon);
                     button.appendChild(document.createTextNode(text));
+                    button.disabled = socketAction !== CrawlActions.open;
 
                     button.dataset['tooltip'] = tooltip;
                     if (disabled) {
@@ -106,51 +102,105 @@ export class StarWarsCrawl implements IOgModule {
                     action: target.dataset.socketAction,
                     payload: {
                         crawlId: target.dataset.url,
+                        name: target.dataset.ogCrawlPlayerName,
                     },
                 });
+                document.dispatchEvent(new Event(`${namespace}:${enricherName}:${target.dataset.socketAction}`));
             }
+        });
+
+        document.addEventListener(`${namespace}:${enricherName}:${CrawlActions.open}`, () => {
+            document
+                .querySelectorAll(`.og-crawl-button-${CrawlActions.play}`)
+                .forEach((el) => ((el as HTMLButtonElement).disabled = false));
+            document
+                .querySelectorAll(`.og-crawl-button-${CrawlActions.close}`)
+                .forEach((el) => ((el as HTMLButtonElement).disabled = false));
+            document.querySelectorAll(`.og-crawl-button-${CrawlActions.open}`).forEach((el) => ((el as HTMLButtonElement).disabled = true));
+        });
+        document.addEventListener(`${namespace}:${enricherName}:${CrawlActions.close}`, () => {
+            document.querySelectorAll(`.og-crawl-button-${CrawlActions.play}`).forEach((el) => ((el as HTMLButtonElement).disabled = true));
+            document
+                .querySelectorAll(`.og-crawl-button-${CrawlActions.close}`)
+                .forEach((el) => ((el as HTMLButtonElement).disabled = true));
+            document
+                .querySelectorAll(`.og-crawl-button-${CrawlActions.open}`)
+                .forEach((el) => ((el as HTMLButtonElement).disabled = false));
         });
 
         logText('StarWarsCrawl initiated');
     }
     ready(): void {
         logText('StarWarsCrawl is getting ready');
-        this.ogGameModuleSocket.registerAction<CrawlPayload>('open', loadCrawl);
-        this.ogGameModuleSocket.registerAction<CrawlPayload>('close', unloadCrawl);
+        this.ogGameModuleSocket.registerAction<CrawlPayload>(CrawlActions.open, this.loadCrawl);
+        this.ogGameModuleSocket.registerAction<CrawlPayload>(CrawlActions.close, this.unloadCrawl);
         logText('StarWarsCrawl is ready');
     }
-}
 
-async function loadCrawl(payload: CrawlPayload) {
-    if (hasIframe()) {
-        logText('The iframe is already there; cannot add.');
-        return;
-    }
-    const url = `https://crawls.rpg.solutions/crawls/play/${payload.crawlId}`;
-    iframe.src = url;
-    iframe.addEventListener('load', (e) => {
-        logText('Crawl iframe is loaded');
-        if (iframe.contentWindow == null) {
-            logError('iframe.contentWindow is null');
+    loadCrawl(payload: CrawlPayload) {
+        if (hasIframe()) {
+            logText('The iframe is already there; cannot add.');
             return;
         }
-    });
-    document.body.appendChild(iframe);
+        const url = crawlUrlPrefix + payload.crawlId;
+        iframe.src = url;
+        iframe.addEventListener('load', (e) => {
+            logText('Crawl iframe is loaded');
+            if (iframe.contentWindow == null) {
+                logError('iframe.contentWindow is null');
+                return;
+            }
+        });
+        document.body.appendChild(iframe);
 
-    // GM can reload everyone's UI
-    if ((game as Game).user?.isGM) {
-        logText('GM');
-        document.body.appendChild(closeButton);
+        addGMButtonBar(payload.name);
+        logText('loadCrawl', payload);
+    }
+
+    unloadCrawl(payload: CrawlPayload) {
+        if (!hasIframe()) {
+            logText('The iframe is not present; cannot remove.');
+            return;
+        }
+        document.body.removeChild(iframe);
+
+        removeGMButtonBar(payload.name);
+        logText('unloadCrawl', payload);
     }
 }
 
-async function unloadCrawl(payload: CrawlPayload) {
-    if (!hasIframe()) {
-        logText('The iframe is not present; cannot remove.');
+function addGMButtonBar(name: string) {
+    if (!(game as Game).user?.isGM) {
         return;
     }
-    document.body.removeChild(iframe);
-    document.body.removeChild(closeButton);
+    logText('addButtonBar');
+
+    const buttonBar = findButtonBar(name);
+    const controlsBar = wrapButtonBarWithContainer(buttonBar);
+    document.body.appendChild(controlsBar);
+}
+function removeGMButtonBar(name: string) {
+    if (!(game as Game).user?.isGM) {
+        return;
+    }
+    logText('removeButtonBar');
+
+    const controlsBar = document.getElementById('og-crawl-controls-container');
+    document.body.removeChild(controlsBar as Node);
+}
+function wrapButtonBarWithContainer(buttonBar: HTMLElement): HTMLElement {
+    const container = document.createElement('div');
+    container.id = 'og-crawl-controls-container';
+    container.style.zIndex = '1000';
+    container.style.position = 'absolute';
+    container.style.top = '0';
+    container.style.right = '0';
+    container.appendChild(buttonBar.cloneNode(true));
+    return container;
+}
+function findButtonBar(name: string): HTMLElement {
+    const bar = document.querySelector(`.og-crawl-player[data-og-crawl-player-name="${name}"]`);
+    return bar as HTMLElement;
 }
 
 function hasIframe() {
@@ -165,4 +215,10 @@ function hasIframe() {
 
 interface CrawlPayload {
     crawlId: string;
+    name: string;
+}
+enum CrawlActions {
+    'open' = 'open',
+    'play' = 'play',
+    'close' = 'close',
 }
