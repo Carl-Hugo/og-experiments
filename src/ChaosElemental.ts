4a1e83d8147418365464fda6361cd9a914b88bcf
Carl-Hugo Marcotte <carlhugo@gmail.com>
2024-09-30 14:53:01 -0400

    Fix Chaos Elemental UI and interactions

--- src/ChaosElemental.ts
+++ src/ChaosElemental.ts
@@ -1,5 +1,9 @@
+// @ts-nocheck
 import { OgBaseModule } from './IModule';
+import { OgExperiment } from './OgExperiments';
 import { registerGameExtensions } from './utils';
+
+const foundryModuleEventName = `module.${OgExperiment.namespace}`;
 
 export class ChaosElemental extends OgBaseModule {
     public get name(): string {
@@ -9,18 +13,7 @@ export class ChaosElemental extends OgBaseModule {
     public override ready(): void {
         const tokenId = 'aXi4DRE5JhxQvWFl';
         const tableId = '5jfwDOTBmXa88fzY';
-
-        const showButton = () => {
-            // Create the initial chat message with the "Try the chaos" button
-            ChatMessage.create({
-                content: `<button class="roll-the-chaos">Try the chaos</button>`,
-                speaker: ChatMessage.getSpeaker(),
-            });
-        };
-
-        registerGameExtensions('chaosElemental', {
-            showButton: showButton,
-        });
+        const tokenAlias = 'Chaos Elemental';
 
         // Event listener for chat messages
         Hooks.on('renderChatMessage', (message, html, data) => {
@@ -31,15 +24,17 @@ export class ChaosElemental extends OgBaseModule {
                 // Roll a d100
                 let roll = await new Roll('1d100').roll({ async: true });
 
+                // Create the speaker object
+                const speaker = { alias: tokenAlias };
+
                 // Send the roll result to chat
                 await roll.toMessage({
-                    speaker: ChatMessage.getSpeaker(),
+                    speaker: speaker,
                     flavor: "Trying the chaos...<p>Let's see if the Chaos Elemental changes form (50% chance)!</p>",
                 });
 
                 // Request the GM to delete the chat message containing the button
-                // @ts-ignore
-                game.socket.emit('module.chaosElemental', {
+                game.socket.emit(foundryModuleEventName, {
                     action: 'deleteMessage',
                     messageId: message.id,
                 });
@@ -49,14 +44,14 @@ export class ChaosElemental extends OgBaseModule {
                     const chatContent = `<button class="roll-next-chaos">What will be my next form?</button>`;
                     ChatMessage.create({
                         content: chatContent,
-                        speaker: ChatMessage.getSpeaker(),
+                        speaker: speaker,
                         flavor: 'Roll what the next form of the Chaos Elemental will be...',
                     });
                 } else {
                     // Send a new chat message saying that nothing happens
                     ChatMessage.create({
                         content: 'Nothing happens.',
-                        speaker: ChatMessage.getSpeaker(),
+                        speaker: speaker,
                     });
                 }
             });
@@ -66,32 +61,48 @@ export class ChaosElemental extends OgBaseModule {
                 event.preventDefault();
 
                 // Request the GM to delete the chat message containing the button
-                // @ts-ignore
-                game.socket.emit('module.chaosElemental', {
+                game.socket.emit(foundryModuleEventName, {
                     action: 'deleteMessage',
                     messageId: message.id,
                 });
 
+                // Create the speaker object
+                const speaker = { alias: tokenAlias };
+
                 // Roll on the roll table
-                // @ts-ignore
                 const table = game.tables.get(tableId);
                 if (table) {
-                    await table.draw({ rollMode: 'roll', speaker: ChatMessage.getSpeaker() });
+                    await table.draw({ rollMode: 'roll', speaker: speaker });
                 } else {
-                    // @ts-ignore
                     ui.notifications.warn(`RollTable with ID "${tableId}" not found.`);
                 }
             });
         });
 
+        // Hook to create the button at the beginning of each new combat round
+        Hooks.on('updateCombat', (combat, updateData, options, userId) => {
+            if (game.user.isGM && 'round' in updateData) {
+                // Create the speaker object
+                const speaker = { alias: tokenAlias };
+
+                // Create the chat message with the "Try the chaos" button
+                const chatContent = `<button class="roll-the-chaos">Try the chaos</button>`;
+                ChatMessage.create({
+                    content: chatContent,
+                    speaker: speaker,
+                    flavor: "A new round has begun, let's see if the Chaos Elemental changes its form!",
+                });
+            }
+        });
+
         // Socket listener for the GM to delete messages
-        // @ts-ignore
         if (game.user.isGM) {
-            // @ts-ignore
-            game.socket.on('module.chaosElemental', async (data) => {
+            game.socket.on(foundryModuleEventName, async (data) => {
+                this.logDebug(foundryModuleEventName, data);
                 if (data.action === 'deleteMessage') {
-                    // @ts-ignore
+                    this.logDebug('module.chaos-elemental deleteMessage messageId: ', data.messageId);
                     let message = game.messages.get(data.messageId);
+                    this.logDebug('module.chaos-elemental deleteMessage message: ', message);
                     if (message) {
                         await message.delete();
                     }
@@ -99,20 +110,19 @@ export class ChaosElemental extends OgBaseModule {
             });
         }
 
-        // Hook to create the button at the beginning of each new combat round
-        // @ts-ignore
-        Hooks.on('updateCombat', (combat, updateData, options, userId) => {
-            this.logDebug('updateCombat', combat, updateData, options, userId);
-            // @ts-ignore
-            if (game.user.isGM && 'round' in updateData) {
-                // Ensure the token exists in the current scene
-                // @ts-ignore
-                const token = canvas.tokens.get(tokenId);
-                if (token) {
-                    // Create the chat message with the "Try the chaos" button
-                    showButton();
-                }
-            }
+        registerGameExtensions('chaosElemental', {
+            showButton: function (tokenId: string) {
+                // Create the speaker object
+                const speaker = { alias: tokenAlias };
+
+                // Create the initial chat message with the "Try the chaos" button
+                const chatContent = `<button class="roll-the-chaos">Try the chaos</button>`;
+
+                ChatMessage.create({
+                    content: chatContent,
+                    speaker: speaker,
+                });
+            },
         });
     }
 }
